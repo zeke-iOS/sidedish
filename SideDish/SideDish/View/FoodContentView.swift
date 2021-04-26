0c5f1100c7f709deda893e8e1389064a9b6f87d8
ellyheetov <yaaajcm@naver.com>
2021-04-26 17:21:09 +0900

    feat: add FoodContentView for reusable

--- SideDish/SideDish/View/FoodContentView.swift
+++ SideDish/SideDish/View/FoodContentView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+@IBDesignable
 class FoodContentView: UIView {
     var stack : UIStackView = {
         let stackView = UIStackView()
@@ -15,22 +16,32 @@ class FoodContentView: UIView {
     }()
     var title : UILabel = {
         let label = UILabel()
-        label.text = "title"
+        label.text = "음식 이름"
+        label.textColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        label.font = UIFont(name: "NotoSansKR-Bold", size: 16)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
         return label
     }()
     var detail : UILabel = {
         let label = UILabel()
-        label.text = "detail"
+        label.text = "아무거나 먹어"
+        label.textColor = UIColor(red: 0.51, green: 0.51, blue: 0.51, alpha: 1)
+        label.font = UIFont(name: "NotoSansKR-Regular", size: 14)
         return label
     }()
     var discountPrice : UILabel = {
         let label = UILabel()
-        label.text = "discount"
+        label.text = "6000원"
+        label.textColor = UIColor(red: 0.004, green: 0.004, blue: 0.004, alpha: 1)
+        label.font = UIFont(name: "NotoSansKR-Bold", size: 14)
         return label
     }()
     var originalPrice : UILabel = {
         let label = UILabel()
-        label.text = "original"
+        label.text = "7000"
+        label.textColor = UIColor(red: 0.741, green: 0.741, blue: 0.741, alpha: 1)
+        label.font = UIFont(name: "NotoSansKR-Bold", size: 14)
         return label
     }()
     var badgeView : UIStackView = {
@@ -52,6 +63,8 @@ class FoodContentView: UIView {
         self.addSubview(stack)
         
         stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.alignment = .leading
+        stack.distribution = .fill
         stack.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor).isActive = true
         stack.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor).isActive = true
         stack.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor).isActive = true
@@ -59,8 +72,56 @@ class FoodContentView: UIView {
         
         self.stack.addArrangedSubview(title)
         self.stack.addArrangedSubview(detail)
-        self.stack.addArrangedSubview(discountPrice)
-        self.stack.addArrangedSubview(originalPrice)
+        
+        let priceStack = UIStackView()
+        priceStack.axis = .horizontal
+        priceStack.spacing = 4
+        priceStack.alignment = .fill
+        priceStack.distribution = .fillProportionally
+        priceStack.addArrangedSubview(discountPrice)
+        priceStack.addArrangedSubview(originalPrice)
+        
+        self.stack.addArrangedSubview(priceStack)
         self.stack.addArrangedSubview(badgeView)
+    }
+    func setText(with card : Card?){
+        guard let card = card else { return }
+        
+        self.title.text = card.title
+        self.detail.text = card.detail
+        self.discountPrice.text = card.discountPrice?.description
+        self.originalPrice.attributedText = NSMutableAttributedString(
+            string: card.originalPrice?.description ?? "",
+            attributes: [NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue])
+        guard let badges = card.badge else {
+            badgeView.isHidden = true
+            return
+        }
+        badges.forEach{ badge in
+            let view = makeBadge(with: badge)
+            self.badgeView.addArrangedSubview(view)
+        }
+    }
+    func makeBadge(with name: String) -> UIView {
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 72, height: 25))
+        
+        view.backgroundColor = .white
+        view.layer.backgroundColor = #colorLiteral(red: 0.5095996261, green: 0.8290402293, blue: 0.1742436588, alpha: 1)
+        view.layer.cornerRadius = 5
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 56, height: 17))
+        label.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        label.font = UIFont(name: "NotoSansKR-Bold", size: 12)
+        label.text = name
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.widthAnchor.constraint(equalToConstant: 56).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 17).isActive = true
+        label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: view.topAnchor, constant: 4).isActive = true
+     
+        return view
     }
 }
